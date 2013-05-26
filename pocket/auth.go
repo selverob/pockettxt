7a7e2bc818d71734a36989b04f3a28ca3ddf6d61
Róbert Selvek <robert.selvek@gmail.com>
2013-05-26 22:11:00 +0200

    Completed Pocket authorization

--- pocket/auth.go
+++ pocket/auth.go
@@ -8,26 +8,69 @@ import (
 	"net/http"
 )
 
+const (
+	redirURI = "127.0.0.1"
+)
+
 func Auth(cKey string) (token string, err error) {
+	fmt.Println("Beginning authentication")
 	rToken, err := requestToken(cKey)
 	if err != nil {
-		err = fmt.Errorf("couldn't get request token: %v", err)
+		return
 	}
-	fmt.Println(rToken)
+
+	url := fmt.Sprintf("https://getpocket.com/auth/authorize?request_token=%s&redirect_uri=%s", rToken, redirURI)
+	fmt.Println("Please open the following URL in your browser, log in and then press enter")
+	fmt.Println(url)
+
+	var str string
+	fmt.Scanf("%s", &str)
+	fmt.Println("Continuing authetication")
+
+	token, err = accessToken(cKey, rToken)
+	if err != nil {
+		return
+	}
 	return
 }
 
 func requestToken(cKey string) (rToken string, err error) {
-	data, err := json.Marshal(map[string]string{
+	data := map[string]string{
 		"consumer_key": cKey,
-		"redirect_url": "http://sellweek.github.io/pockettxt",
-	})
+		"redirect_uri": redirURI,
+	}
+
+	r, err := requestChain(data, "/v3/oauth/request")
+	if err != nil {
+		fmt.Errorf("couldn't get a request token: %v", err)
+	}
+
+	rToken = r["code"]
+	return
+}
+
+func accessToken(cKey, rToken string) (aToken string, err error) {
+	data := map[string]string{
+		"consumer_key": cKey,
+		"code":         rToken,
+	}
+	r, err := requestChain(data, "/v3/oauth/authorize")
+	if err != nil {
+		err = fmt.Errorf("couldn't get the access token: %v", err)
+	}
+
+	aToken = r["access_token"]
+	return
+}
+
+func requestChain(data map[string]string, url string) (response map[string]string, err error) {
+	jsData, err := json.Marshal(data)
 	if err != nil {
 		err = fmt.Errorf("couldn't marshal data: %v", err)
 		return
 	}
 
-	r, err := http.NewRequest("POST", "http://getpocket.com/v3/oauth/request", bytes.NewBuffer(data))
+	r, err := http.NewRequest("POST", "http://getpocket.com"+url, bytes.NewBuffer(jsData))
 	if err != nil {
 		err = fmt.Errorf("couldn't create request: %v", err)
 		return
@@ -53,13 +96,12 @@ func requestToken(cKey string) (rToken string, err error) {
 		return
 	}
 
-	m := make(map[string]string, 1)
-	err = json.Unmarshal(body, &m)
+	response = make(map[string]string)
+	err = json.Unmarshal(body, &response)
 	if err != nil {
 		err = fmt.Errorf("couldn't unmarshal response: %v", err)
 		return
 	}
 
-	rToken = m["code"]
 	return
 }
